8f4c0f131452e4ee623bbf426b402b5c9e31045b
Adejare Faruk <farukpamilerin@gmail.com>
2025-10-15 10:07:38 +0100

    update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -19,12 +19,12 @@ CREATE TABLE Customers (
 ),
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
-    customer_id FOREIGN KEY(customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE,
     order_date DATE NOT NULL,
 ),
 CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
-    order_id FOREIGN KEY REFERENCES Orders(order_id),
-    book_id FOREIGN KEY REFERENCES Books(book_id),
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id),
     quantity DOUBLE NOT NULL,
 );
