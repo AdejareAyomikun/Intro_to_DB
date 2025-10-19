174e886ab41693b142268b2b90904e6c5192b90e
Adejare Faruk <farukpamilerin@gmail.com>
2025-10-19 15:40:16 +0100

    Added task_2.sql

---
+++ task_2.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id foreign key REFERENCES Authors(author_id) ON DELETE CASCADE,
+    price VARCHAR(50) NOT NULL,
+    publication_date DATE,
+),
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL,
+),
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+),
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE NOT NULL,
+),
+CREATE TABLE Order_Details(
+    orderdetailid INT PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE NOT NULL,
+);
