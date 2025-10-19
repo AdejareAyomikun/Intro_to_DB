1ccac912d143f90fc04c4d4be7ffa43a67c8d69e
Adejare Faruk <farukpamilerin@gmail.com>
2025-10-19 15:57:09 +0100

    Added task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -2,5 +2,5 @@
 -- The database name should be passed to the mysql client, for example:
 --   mysql -u root -p alx_book_store < .\task_5.sql
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
